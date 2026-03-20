90ac2ca5dcfd70d9417b79f8ab7b4fc4e4873add
Susanta Kumar Das <susanta.das@bluehorse.in>
2026-03-20 20:42:42 +0530

    Added To-Do frontend page

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app.secret_key = "secret123"
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('todo.html')
 @app.route('/api')
 def api():
     with open('file.json') as f:
